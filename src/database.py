47e47e40ed398a7e0448110286820f30f45a20b4
mathisdlg <delage.mathis.1@gmail.com>
2025-10-06 14:05:09 +0200

    Refactor database: remove guild table and update channels table structure

--- src/database.py
+++ src/database.py
@@ -4,19 +4,10 @@ def init_database(db_path):
     database_connection = sqlite3.connect(db_path)
     database_cursor = database_connection.cursor()
 
-    # Create guild table
-    database_cursor.execute("""
-        CREATE TABLE IF NOT EXISTS guild (
-            guild_id INTEGER PRIMARY KEY
-        );
-    """)
-
-    # Create channels table (linked to guild)
+    # Create channels table
     database_cursor.execute("""
         CREATE TABLE IF NOT EXISTS channels (
-            channel_id INTEGER PRIMARY KEY,
-            guild_id INTEGER NOT NULL,
-            FOREIGN KEY (guild_id) REFERENCES guild (guild_id)
+            channel_id INTEGER PRIMARY KEY
         );
     """)
 
@@ -83,17 +74,12 @@ def remove_channel(cursor, channel_id):
     cursor.execute("DELETE FROM channels WHERE channel_id = ?", (channel_id,))
     cursor.connection.commit()
 
-def get_channels(cursor):
-    cursor.execute("SELECT channel_id FROM channels")
+def get_channels(cursor, channel_id=None):
+    if channel_id:
+        cursor.execute("SELECT channel_id FROM channels WHERE channel_id = ?", (channel_id,))
+    else:
+        cursor.execute("SELECT channel_id FROM channels")
     return cursor.fetchall()
-
-def add_guild(cursor, guild_id):
-    cursor.execute("INSERT INTO guild (guild_id) VALUES (?)", (guild_id,))
-    cursor.connection.commit()
-
-def remove_guild(cursor, guild_id):
-    cursor.execute("DELETE FROM guild WHERE guild_id = ?", (guild_id,))
-    cursor.connection.commit()
 
 def add_user(cursor, user_id, username):
     cursor.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)", (user_id, username))
