b4cdae22da26180ce853c6a1881c25bcd4dc5943
mathisdlg <delage.mathis.1@gmail.com>
2025-10-06 14:20:20 +0200

    Refactor database connection handling in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 
 import discord, asyncio
 from commands import handle_message
-from database import init_database, close_database
-from utils import load_token, CONNECTION, DATABASE_PATH
+from database import Database
+from utils import load_token, DATABASE_PATH
 
 def main():
     intents = discord.Intents(581068273470528).default() # Intents: 581068273470528
@@ -12,13 +12,13 @@ def main():
 
     client = discord.Client(intents=intents)
 
-    CONNECTION, cursor = init_database(DATABASE_PATH)
+    CONNECTION: Database = Database(DATABASE_PATH)
 
     @client.event
     async def on_message(message: discord.Message):
         if message.author == client.user:
             return
-        await handle_message(message)
+        await handle_message(CONNECTION, message)
 
     @client.event
     async def on_ready():
@@ -38,6 +38,6 @@ if __name__ == "__main__":
         asyncio.run(main())
     except Exception as e:
         if CONNECTION:
-            close_database(CONNECTION)
+            CONNECTION.close_database()
         print(f"Error: {e}")
         print("Exiting...")
