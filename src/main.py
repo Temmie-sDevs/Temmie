04187a98f527a90eca76e72c3d99a8a09d3792a1
mathisdlg <delage.mathis.1@gmail.com>
2025-10-06 14:28:56 +0200

    Modify the function that is responsible for closing the database

--- src/main.py
+++ src/main.py
@@ -6,38 +6,41 @@ from database import Database
 from utils import load_token, DATABASE_PATH
 
 def main():
-    intents = discord.Intents(581068273470528).default() # Intents: 581068273470528
-    intents.members = True
-    intents.message_content = True
+    try:
+        intents = discord.Intents(581068273470528).default() # Intents: 581068273470528
+        intents.members = True
+        intents.message_content = True
 
-    client = discord.Client(intents=intents)
+        client = discord.Client(intents=intents)
 
-    CONNECTION: Database = Database(DATABASE_PATH)
+        CONNECTION: Database = Database(DATABASE_PATH)
 
-    @client.event
-    async def on_message(message: discord.Message):
-        if message.author == client.user:
-            return
-        await handle_message(CONNECTION, message)
+        @client.event
+        async def on_message(message: discord.Message):
+            if message.author == client.user:
+                return
+            await handle_message(CONNECTION, message)
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is connected to the following guild:\n')
-        for guild in client.guilds:
-            print(f'{guild.name} (id: {guild.id})')
+        @client.event
+        async def on_ready():
+            print(f'{client.user} is connected to the following guild:\n')
+            for guild in client.guilds:
+                print(f'{guild.name} (id: {guild.id})')
 
-    token = load_token()
-    if token:
-        client.run(token)
-    else:
-        raise FileNotFoundError("Token file not found")
+        token = load_token()
+        if token:
+            client.run(token)
+        else:
+            raise FileNotFoundError("Token file not found")
+    finally:
+        if CONNECTION:
+            CONNECTION.close_database()
+        print("Database connection closed.")
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except Exception as e:
-        if CONNECTION:
-            CONNECTION.close_database()
         print(f"Error: {e}")
         print("Exiting...")
